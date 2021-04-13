99a23119a6f81167d20349f7020ba6b93cbea1ea
Nira <Mattias.smedman@gmail.com>
2021-04-13 20:11:16 +0200

    Code cleanup

--- Pokemon Roguelite/Assets/Scripts/UI/AttackDisplay.cs
+++ Pokemon Roguelite/Assets/Scripts/UI/AttackDisplay.cs
@@ -8,7 +8,6 @@ public class AttackDisplay : MonoBehaviour
     [SerializeField]
     GameObject attackDisplayPrefab;
 
-    Pokemon oldPokemon;
     // Start is called before the first frame update
     void Start()
     {
