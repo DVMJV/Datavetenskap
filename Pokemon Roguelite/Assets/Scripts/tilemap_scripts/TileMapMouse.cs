e5437f7e4aea745647d432b21189cd4a541e6a30
Rydsund <v.rydsund@gmail.com>
2021-04-14 11:53:51 +0200

    Added mouse highlight.

--- Pokemon Roguelite/Assets/Scripts/tilemap_scripts/TileMapMouse.cs
+++ Pokemon Roguelite/Assets/Scripts/tilemap_scripts/TileMapMouse.cs
@@ -2,19 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(TileMap))]
 public class TileMapMouse : MonoBehaviour
 {
-    public Color highlightColor;
-    Color defaultColor;
+    public Transform selectionCube;
+    TileMap tileMap;
     Collider coll;
-    Renderer rend;
+    Vector3 tilePosHit;
+    Vector3 currentTileCoord;
+  
 
     private void Start()
     {
         coll = GetComponent<Collider>();
-        rend = GetComponent<Renderer>();
-        defaultColor = rend.material.color;
-        highlightColor = Color.cyan;
+        tileMap = GetComponent<TileMap>();
     }
 
     void Update()
@@ -25,11 +26,25 @@ public class TileMapMouse : MonoBehaviour
         if (coll.Raycast(ray, out hitInfo, Mathf.Infinity))
         {
             // Returns correct local position regardless of mesh position and rotation.
-            Debug.Log(transform.worldToLocalMatrix.MultiplyPoint3x4(hitInfo.point));
+            tilePosHit = transform.worldToLocalMatrix.MultiplyPoint3x4(hitInfo.point);
+            int x = Mathf.FloorToInt(tilePosHit.x / tileMap.tileSize);
+            int z = Mathf.FloorToInt(tilePosHit.z / tileMap.tileSize);
+            currentTileCoord.x = x;
+            currentTileCoord.z = z;
+            // currentTileCoord.y  // Height data from tilemap.
+
+            selectionCube.GetComponent<MeshRenderer>().enabled = true;
+            selectionCube.transform.position = currentTileCoord * tileMap.tileSize;
+            selectionCube.transform.position += new Vector3(tileMap.tileSize / 2, 0, tileMap.tileSize / 2);                                           
+            //Debug.Log("Tile:" + x + ", " + z);
         }
         else
         {
-           // rend.material.color = defaultColor;
+            selectionCube.GetComponent<MeshRenderer>().enabled = false;
+        }
+        if (Input.GetMouseButtonDown(0)) // && target != null.
+        {
+
         }
     }
 }
