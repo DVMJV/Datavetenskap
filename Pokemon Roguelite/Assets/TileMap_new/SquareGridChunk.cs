713da60b31d7e09490fed7e1f139bad132fa60de
Rydsund <v.rydsund@gmail.com>
2021-04-29 12:12:40 +0200

    Refactored code
    Moved methods and renamed some variables

--- Pokemon Roguelite/Assets/TileMap_new/SquareGridChunk.cs
+++ Pokemon Roguelite/Assets/TileMap_new/SquareGridChunk.cs
@@ -4,22 +4,20 @@ using UnityEngine.UI;
 public class SquareGridChunk : MonoBehaviour
 {
     SquareCell[] cells;
-    SquareMesh squareMesh;
+    public SquareMesh terrain;
     Canvas gridCanvas;
     bool enabled;
 
     private void Awake()
     {
         gridCanvas = GetComponentInChildren<Canvas>();
-        squareMesh = GetComponentInChildren<SquareMesh>();
-
         cells = new SquareCell[SquareMetrics.chunkSizeX * SquareMetrics.chunkSizeZ];
         ShowUI(false);
     }
 
     private void LateUpdate()
     {
-        squareMesh.Triangulate(cells);
+        Triangulate();
         enabled = false;
     }
 
@@ -40,5 +38,96 @@ public class SquareGridChunk : MonoBehaviour
     public void ShowUI(bool visible) 
     {
         gridCanvas.gameObject.SetActive(visible);
+    }
+
+    public void Triangulate()
+    {
+        terrain.Clear();
+        for (int i = 0; i < cells.Length; i++)
+        {
+            Triangulate(cells[i]);
+        }
+        terrain.Apply();
+    }
+
+    void Triangulate(SquareCell cell)
+    {
+        Vector3 center = cell.transform.localPosition;  // Counter clockwise...
+        terrain.AddTriangle(center + SquareMetrics.corners[0], center + SquareMetrics.corners[1], center + SquareMetrics.corners[2]);
+        terrain.AddTriangle(center + SquareMetrics.corners[0], center + SquareMetrics.corners[2], center + SquareMetrics.corners[3]);
+        terrain.AddTriangleColor(cell.Color);
+        terrain.AddTriangleColor(cell.Color); // Done twice since it is a square.
+
+        for (int i = 0; i < 4; i++)
+        {
+            SquareCell neighbor = cell.GetNeighbor((SquareDirection)i);
+
+            if (neighbor == null)
+            {
+                Vector3 elevationDifference = new Vector3(0, (cell.Elevation + 2) * SquareMetrics.elevationStep, 0);
+                if (i == 0)
+                {
+                    terrain.AddQuad(center + SquareMetrics.corners[1],
+                            center + SquareMetrics.corners[0],
+                            center + SquareMetrics.corners[0] - elevationDifference,
+                            center + SquareMetrics.corners[1] - elevationDifference);
+                }
+                else if (i == 1)
+                {
+                    terrain.AddQuad(center + SquareMetrics.corners[2],
+                            center + SquareMetrics.corners[1],
+                            center + SquareMetrics.corners[1] - elevationDifference,
+                            center + SquareMetrics.corners[2] - elevationDifference);
+                }
+                else if (i == 2)
+                {
+                    terrain.AddQuad(center + SquareMetrics.corners[3],
+                            center + SquareMetrics.corners[2],
+                            center + SquareMetrics.corners[2] - elevationDifference,
+                            center + SquareMetrics.corners[3] - elevationDifference);
+                }
+                else
+                {
+                    terrain.AddQuad(center + SquareMetrics.corners[0],
+                    center + SquareMetrics.corners[3],
+                    center + SquareMetrics.corners[3] - elevationDifference,
+                    center + SquareMetrics.corners[0] - elevationDifference);
+                }
+                terrain.AddQuadColor(cell.Color);
+            }
+            else if (cell.Elevation - neighbor.Elevation > 0)
+            {
+                Vector3 neighborCenter = neighbor.transform.localPosition;
+                if (i == 0)
+                {
+                    terrain.AddQuad(center + SquareMetrics.corners[1],
+                           center + SquareMetrics.corners[0],
+                           neighborCenter + SquareMetrics.corners[3],
+                           neighborCenter + SquareMetrics.corners[2]);
+                }
+                else if (i == 1)
+                {
+                    terrain.AddQuad(center + SquareMetrics.corners[2],
+                           center + SquareMetrics.corners[1],
+                           neighborCenter + SquareMetrics.corners[0],
+                           neighborCenter + SquareMetrics.corners[3]);
+                }
+                else if (i == 2)
+                {
+                    terrain.AddQuad(center + SquareMetrics.corners[3],
+                           center + SquareMetrics.corners[2],
+                           neighborCenter + SquareMetrics.corners[1],
+                           neighborCenter + SquareMetrics.corners[0]);
+                }
+                else
+                {
+                    terrain.AddQuad(center + SquareMetrics.corners[0],
+                    center + SquareMetrics.corners[3],
+                    neighborCenter + SquareMetrics.corners[2],
+                    neighborCenter + SquareMetrics.corners[1]);
+                }
+                terrain.AddQuadColor(cell.Color);
+            }
+        }
     }
 }
--- Pokemon Roguelite/Assets/TileMap_new/SquareMesh.cs
+++ Pokemon Roguelite/Assets/TileMap_new/SquareMesh.cs
@@ -24,121 +24,31 @@ public class SquareMesh : MonoBehaviour
         squareMesh.name = "Square Mesh";
     }
 
-    // Loops trough all the cells triangulating them individually.
-    // After that is done assign the generated vertices and triangles to the mesh and calculate the normals.
-    public void Triangulate(SquareCell[] cells)
+    public void Clear() 
     {
         squareMesh.Clear();
         verticies.Clear();
         colors.Clear();
         triangles.Clear();
+    }
 
-        for (int i = 0; i < cells.Length; i++)
-        {
-            Triangulate(cells[i]);
-        }
-
-        squareMesh.vertices = verticies.ToArray();
-        squareMesh.colors = colors.ToArray();
-        squareMesh.triangles = triangles.ToArray();
-        
+    public void Apply()
+    {
+        squareMesh.SetVertices(verticies);
+        squareMesh.SetColors(colors);
+        squareMesh.SetTriangles(triangles, 0);
         squareMesh.RecalculateNormals();
-        meshCollider.sharedMesh= squareMesh;
+        meshCollider.sharedMesh = squareMesh;
     }
 
-    void Triangulate(SquareCell cell) 
-    {
-        Vector3 center = cell.transform.localPosition;  // Counter clockwise...
-        AddTriangle(center + SquareMetrics.corners[0], center + SquareMetrics.corners[1], center + SquareMetrics.corners[2]);
-        AddTriangle(center + SquareMetrics.corners[0], center + SquareMetrics.corners[2], center + SquareMetrics.corners[3]);
-        AddTriangleColor(cell.Color);
-        AddTriangleColor(cell.Color); // Done twice since it is a square.
-
-
-        for (int i = 0; i < 4; i++)
-        {
-            SquareCell neighbor = cell.GetNeighbor((SquareDirection)i);
-            
-            if (neighbor == null)
-            {
-                Vector3 elevationDifference = new Vector3(0, (cell.Elevation + 2) * SquareMetrics.elevationStep, 0);
-                if (i == 0)
-                {
-                    AddQuad(center + SquareMetrics.corners[1],
-                            center + SquareMetrics.corners[0],
-                            center + SquareMetrics.corners[0] - elevationDifference,
-                            center + SquareMetrics.corners[1] - elevationDifference);
-                }
-                else if (i == 1)
-                {
-                    AddQuad(center + SquareMetrics.corners[2],
-                            center + SquareMetrics.corners[1],
-                            center + SquareMetrics.corners[1] - elevationDifference,
-                            center + SquareMetrics.corners[2] - elevationDifference);
-                }
-                else if (i == 2)
-                {
-                    AddQuad(center + SquareMetrics.corners[3],
-                            center + SquareMetrics.corners[2],
-                            center + SquareMetrics.corners[2] - elevationDifference,
-                            center + SquareMetrics.corners[3] - elevationDifference);
-                }
-                else
-                {
-                    AddQuad(center + SquareMetrics.corners[0],
-                    center + SquareMetrics.corners[3],
-                    center + SquareMetrics.corners[3] - elevationDifference,
-                    center + SquareMetrics.corners[0] - elevationDifference);
-                }
-                AddQuadColor(cell.Color);
-
-               
-            }
-
-            else if (cell.Elevation - neighbor.Elevation > 0)
-            {
-                Vector3 neighborCenter = neighbor.transform.localPosition;
-                if (i == 0)
-                {
-                    AddQuad(center + SquareMetrics.corners[1],
-                           center + SquareMetrics.corners[0],
-                           neighborCenter + SquareMetrics.corners[3],
-                           neighborCenter + SquareMetrics.corners[2]);
-                }
-                else if (i == 1)
-                {
-                    AddQuad(center + SquareMetrics.corners[2],
-                           center + SquareMetrics.corners[1],
-                           neighborCenter + SquareMetrics.corners[0],
-                           neighborCenter + SquareMetrics.corners[3]);
-                }
-                else if (i == 2)
-                {
-                    AddQuad(center + SquareMetrics.corners[3],
-                           center + SquareMetrics.corners[2],
-                           neighborCenter + SquareMetrics.corners[1],
-                           neighborCenter + SquareMetrics.corners[0]);
-                }
-                else
-                {
-                    AddQuad(center + SquareMetrics.corners[0],
-                    center + SquareMetrics.corners[3],
-                    neighborCenter + SquareMetrics.corners[2],
-                    neighborCenter + SquareMetrics.corners[1]);
-                }
-                AddQuadColor(cell.Color);
-            }
-        }
-    }
-
-    void AddTriangleColor(Color color) 
+    public void AddTriangleColor(Color color) 
     {
         colors.Add(color);
         colors.Add(color);
         colors.Add(color);
     }
 
-    void AddTriangle(Vector3 v1, Vector3 v2, Vector3 v3) 
+    public void AddTriangle(Vector3 v1, Vector3 v2, Vector3 v3) 
     {
         int vertexIndex = verticies.Count;
         verticies.Add(v1);
@@ -150,7 +60,7 @@ public class SquareMesh : MonoBehaviour
         triangles.Add(vertexIndex + 2);
     }
 
-    void AddQuadColor(Color c)
+    public void AddQuadColor(Color c)
     {
         colors.Add(c);
         colors.Add(c);
@@ -158,7 +68,7 @@ public class SquareMesh : MonoBehaviour
         colors.Add(c);
     }
 
-    void AddQuad(Vector3 topLeft, Vector3 topRight, Vector3 bottomRight, Vector3 bottomLeft) 
+    public void AddQuad(Vector3 topLeft, Vector3 topRight, Vector3 bottomRight, Vector3 bottomLeft) 
     {
         int vertexIndex = verticies.Count;
         verticies.Add(topLeft);
