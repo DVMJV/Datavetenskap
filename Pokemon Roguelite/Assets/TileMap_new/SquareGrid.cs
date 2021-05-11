78514cbe4fa188f27091bf147e00ee5d0d6cd2f1
Nira <Mattias.smedman@gmail.com>
2021-05-11 11:41:26 +0200

    Why is it not pushing reee

--- Pokemon Roguelite/Assets/TileMap_new/SquareGrid.cs
+++ Pokemon Roguelite/Assets/TileMap_new/SquareGrid.cs
@@ -24,7 +24,6 @@ public class SquareGrid : MonoBehaviour
 
     private void Awake()
     {
-        int test3 = 2;
         gridCanvas = GetComponentInChildren<Canvas>();
         squareMesh = GetComponentInChildren<SquareMesh>();
         cells = new SquareCell[height * width];
