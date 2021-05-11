2c515edbdd99064a90f6382a59f706c95205b1e0
Nirase <Mattias.smedman@gmail.com>
2021-05-11 12:04:34 +0200

    ?

--- Pokemon Roguelite/Assets/TileMap_new/SquareGrid.cs
+++ Pokemon Roguelite/Assets/TileMap_new/SquareGrid.cs
@@ -24,7 +24,6 @@ public class SquareGrid : MonoBehaviour
 
     private void Awake()
     {
-        int text = 5;
         gridCanvas = GetComponentInChildren<Canvas>();
         squareMesh = GetComponentInChildren<SquareMesh>();
         cells = new SquareCell[height * width];
