908681019fbd497f8371ddc7bf9cf95fd5563770
Nirase <Mattias.smedman@gmail.com>
2021-05-12 18:49:24 +0200

    Revert "Merge branch 'tilemap_new' into Pokemon-Attacks"
    This reverts commit aa7e18f2536d3260b2df5fb0ee4ea38578c565dc, reversing
    changes made to ebc6558671764979c8d36e80f6f83afabcb48ce7.

--- Pokemon Roguelite/Assets/TileMap_new/SquareCell.cs
+++ Pokemon Roguelite/Assets/TileMap_new/SquareCell.cs
@@ -8,10 +8,7 @@ public class SquareCell : MonoBehaviour
     [SerializeField]
     SquareCell[] neighbors;
 
-    public SquareGridChunk chunk;
     public RectTransform uiRect;
-    
-    public SquareCoordinates coordinates;
 
     public SquareCell PathFrom { get; set; }
 
@@ -34,43 +31,12 @@ public class SquareCell : MonoBehaviour
             UpdateDistanceLabel();
         }
     }
-    
-    float terrainTypeIndex;
-    public float TerrainTypeIndex
-    {
-        get { return terrainTypeIndex; }
-        set
-        {
-            if (terrainTypeIndex == value)
-                return;
 
-            terrainTypeIndex = value;
-            Refresh();
-        }
-    }
-    
-    float terrainTypeIndex;
-    public float TerrainTypeIndex
-    {
-        get { return terrainTypeIndex; }
-        set
-        {
-            if (terrainTypeIndex == value)
-                return;
-
-            terrainTypeIndex = value;
-            Refresh();
-        }
-    }
+    public SquareCoordinates coordinates;
+    public Color color;
 
     public int Elevation { get { return elevation; } 
-        set
-        {
-            if (elevation == value)
-                return;
-
-            elevation = value;
-    
+        set { elevation = value;
             Vector3 position = transform.localPosition;
             position.y = value * SquareMetrics.elevationStep;
             transform.localPosition = position;
@@ -78,8 +44,6 @@ public class SquareCell : MonoBehaviour
             Vector3 uiPosition = uiRect.localPosition;
             uiPosition.z = elevation * -SquareMetrics.elevationStep;
             uiRect.localPosition = uiPosition;
-
-            Refresh();
             } }
 
     int elevation;
@@ -104,20 +68,6 @@ public class SquareCell : MonoBehaviour
     public SquareCell[] GetNeighborArray() 
     {
         return neighbors;    
-    }
-    
-    void Refresh() 
-    {
-        if (chunk)
-        {
-            chunk.Refresh();
-            for (int i = 0; i < neighbors.Length; i++)
-            {
-                SquareCell neighbor = neighbors[i];
-                if (neighbor != null && neighbor.chunk != chunk)
-                    neighbor.chunk.Refresh();
-            }
-        }
     }
 
     public void EnableHighlight(Color color)
