393c9ff0262be6f38d05d6c2154584688974f6d4
Rydsund <v.rydsund@gmail.com>
2021-05-14 13:34:02 +0200

    Land now has budget

--- Pokemon Roguelite/Assets/TileMap_new/SquareMapGenerator.cs
+++ Pokemon Roguelite/Assets/TileMap_new/SquareMapGenerator.cs
@@ -10,6 +10,18 @@ public class SquareMapGenerator : MonoBehaviour
     SquareCellPriorityQueue prioQueue;
     int searchFrontierPhase;
 
+    [Range(0f, 0.5f)]
+    public float jitterProbability = 0.25f;
+
+    [Range(0, 100)]
+    public int chunkSizeMin = 10;
+
+    [Range(20, 100)]
+    public int chunkSizeMax = 40;
+
+    [Range(5, 95)]
+    public int landPercentage = 50;
+
     public void GenerateMap(int x, int z) 
     {
         cellCount = x * z;
@@ -17,15 +29,26 @@ public class SquareMapGenerator : MonoBehaviour
         
         if (prioQueue == null)
             prioQueue = new SquareCellPriorityQueue();
-        
-        RaiseTerrain(30);
+
+
+        CreateLand();
         for (int i = 0; i < cellCount; i++)
         {
             grid.GetCell(i).SearchPhase = 0;
         }
+  
     }
 
-    void RaiseTerrain(int chunkSize) 
+    void CreateLand() 
+    {
+        int landBudget = Mathf.RoundToInt(cellCount * landPercentage * 0.01f);
+        while (landBudget > 0)
+        {
+            landBudget = RaiseTerrain(Random.Range(chunkSizeMin, chunkSizeMax + 1), landBudget);
+        }
+    }
+
+    int RaiseTerrain(int chunkSize, int budget) 
     {
         searchFrontierPhase += 1;
         SquareCell firstCell = GetRandomCell();
@@ -39,8 +62,13 @@ public class SquareMapGenerator : MonoBehaviour
         while (size < chunkSize && prioQueue.Count > 0)
         {
             SquareCell current = prioQueue.Dequeue();
-            current.TerrainTypeIndex = 1;
-            current.Elevation = Random.Range(0, 2);
+            if (current.TerrainTypeIndex == 0) // 0 is water, or will be..
+            {
+                current.TerrainTypeIndex = 1; // randomize?
+                if (--budget == 0)
+                    break;
+            }
+            //current.Elevation = Random.Range(0, 2); // extra
             size++;
 
             for (SquareDirection d = SquareDirection.UP; d <= SquareDirection.LEFT; d++)
@@ -50,13 +78,13 @@ public class SquareMapGenerator : MonoBehaviour
                 {
                     neighbor.SearchPhase = searchFrontierPhase;
                     neighbor.Distance = neighbor.coordinates.DistanceTo(center);
-                    neighbor.SearchHeuristic = Random.value < 0.5f ? 1: 0;
-                   // neighbor.Elevation = 2
+                    neighbor.SearchHeuristic = Random.value < jitterProbability ? 1: 0;
                     prioQueue.Enqueue(neighbor);
                 }
             }
         }
         prioQueue.Clear();
+        return budget;
     }
 
     SquareCell GetRandomCell() 
